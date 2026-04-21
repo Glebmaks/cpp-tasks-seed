025ce082b5ce578fc906126febb0f926a7a1acc4
Глеб Максименков <glebmaksimenkov@MacBook-Air-Gleb.local>
2026-04-22 02:39:28 +0300

    Сделал алгоритмы сортировки и тесты

--- 01.InplaceSorting/sort_test.cpp
+++ 01.InplaceSorting/sort_test.cpp
@@ -1,11 +1,61 @@
 #include <gtest/gtest.h>
+#include <vector>
+#include <algorithm>
 
-// А вот тут collvalue.h не надо, берите просто инты
 #include "sorting.h"
 
-// Но проверьте, что сортировки таки работают...
-
-TEST(FooSuite, FooTest)
+TEST(BubbleSortTest, EmptyVector)
 {
-    EXPECT_EQ(0, 0);
+    std::vector<int> data;
+    bubble_sort(data.begin(), data.end());
+    EXPECT_TRUE(data.empty());
+}
+
+TEST(BubbleSortTest, SingleElement)
+{
+    std::vector<int> data = {52};
+    bubble_sort(data.begin(), data.end());
+    EXPECT_EQ(data, std::vector<int>({52}));
+}
+
+TEST(BubbleSortTest, UnsortedVector)
+{
+    std::vector<int> data = {4, 3, 2, 1};
+    bubble_sort(data.begin(), data.end());
+    EXPECT_TRUE(std::is_sorted(data.begin(), data.end()));
+}
+
+TEST(BubbleSortTest, VectorWithDuplicates)
+{
+    std::vector<int> data = {1, 1, 2, 2, 3, 3};
+    bubble_sort(data.begin(), data.end());
+    EXPECT_TRUE(std::is_sorted(data.begin(), data.end()));
+}
+
+TEST(QuickSortTest, EmptyVector)
+{
+    std::vector<int> data;
+    quick_sort(data.begin(), data.end());
+    EXPECT_TRUE(data.empty());
+}
+
+TEST(QuickSortTest, SingleElement)
+{
+    std::vector<int> data = {52};
+    quick_sort(data.begin(), data.end());
+    EXPECT_EQ(data, std::vector<int>({52}));
+}
+
+TEST(QuickSortTest, UnsortedVector)
+{
+    std::vector<int> data = {4, 3, 2, 1};
+    quick_sort(data.begin(), data.end());
+    EXPECT_TRUE(std::is_sorted(data.begin(), data.end()));
+}
+
+TEST(QuickSortTest, VectorWithDuplicates)
+{
+    std::vector<int> data = {1, 1, 2, 2, 3, 3};
+    quick_sort(data.begin(), data.end());
+    EXPECT_TRUE(std::is_sorted(data.begin(), data.end()));
 }
--- 01.InplaceSorting/sorting.h
+++ 01.InplaceSorting/sorting.h
@@ -1,21 +1,91 @@
 #ifndef SORTING_H
 #define SORTING_H
 
-// Header-only
+#include <iterator>
+#include <utility>
 
-// Для swap
 #include "collvalue.h"
 
-// ----------------------------- Пузырёк (например) --------------------------------------
+// Bubble Sort
+
 template <typename Iterator>
 void bubble_sort(Iterator begin, Iterator end)
 {
+    if (begin == end)
+    {
+        return;
+    }
+
+    for (Iterator last = end; last != begin; --last)
+    {
+        bool swaped = false;
+
+        for (Iterator it = begin; std::next(it) != last; ++it)
+        {
+            Iterator next_it = std::next(it);
+            if (*next_it < *it)
+            {
+                using std::swap;
+                swap(*it, *next_it);
+                swaped = true;
+            }
+        }
+
+        if (!swaped)
+        {
+            break;
+        }
+    }
 }
 
-// --------------------------- QuickSort (например) ---------------------------------------
+// Quick Sort
+
 template <typename Iterator>
 void quick_sort(Iterator begin, Iterator end)
 {
+    if (begin == end || std::next(begin) == end)
+    {
+        return;
+    }
+
+    Iterator left = begin;
+    Iterator right = std::prev(end);
+    auto pivot = *std::next(begin, std::distance(begin, end) / 2);
+
+    while (left <= right)
+    {
+        while (*left < pivot)
+        {
+            ++left;
+        }
+
+        while (pivot < *right)
+        {
+            --right;
+        }
+
+        if (left <= right)
+        {
+            using std::swap;
+            swap(*left, *right);
+            ++left;
+
+            if (right == begin)
+            {
+                break;
+            }
+            --right;
+        }
+    }
+
+    if (begin < right + 1)
+    {
+        quick_sort(begin, right + 1);
+    }
+    if (left < end)
+    {
+        quick_sort(left, end);
+    }
 }
 
 #endif // SORTING_H
